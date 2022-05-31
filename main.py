cedfb05ac362b1e7b09df51b68b2766be15d8538
Chirag <chiragb.garh@gmail.com>
2022-05-31 12:15:23 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+
+while True:
+    game=['rock', 'papers', 'scissors']
+    comp=random.choice(game)
+    player=None
+
+    while player not in game:
+        player=input('rock,papers, or scissors :').lower()
+
+    if player==comp:
+        print('player: ', player)
+        print('computer: ', comp)
+        print('Tie!')
+    elif player=='rock':
+        if comp=='paper':
+            print('player: ', player)
+            print('computer: ', comp)
+            print('You lose!')
+        if comp=='scissors':
+            print('player: ', player)
+            print('computer: ', comp)
+            print('You win!')
+    elif player=='paper':
+        if comp=='rock':
+            print('player: ', player)
+            print('computer: ', comp)
+            print('You win!')
+        if comp=='scissors':
+            print('player: ', player)
+            print('computer: ', comp)
+            print('You lose!')
+    elif player =='scissors':
+        if comp=='rock':
+            print('player: ', player)
+            print('computer: ', comp)
+            print('You lose!')
+        if comp=='paper':
+            print('player: ', player)
+            print('computer: ', comp)
+            print('You win!')
+    again = input('Do you wanna play again? yes/no :').lower()
+
+    if again!='yes':
+        break
+print('Bye!')
+
+
+
+
